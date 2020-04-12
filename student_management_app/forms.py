4dce792969cab034e0c45a83b4741a8afea302b1
sanjeev kumar <sksanjeev362@gmail.com>
2020-04-12 17:30:53 +0530

    Added Staff Session Year Model , Added Take Attendance Page in Staff

--- student_management_app/forms.py
+++ student_management_app/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
-from student_management_app.models import Courses
+from student_management_app.models import Courses, SessionYearModel
+
 
 class DateInput(forms.DateInput):
     input_type = "date"
@@ -12,12 +13,20 @@ class AddStudentForm(forms.Form):
     last_name=forms.CharField(label="Last Name",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     username=forms.CharField(label="Username",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address=forms.CharField(label="Address",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
-
-    courses=Courses.objects.all()
     course_list=[]
+    courses=Courses.objects.all()
+
     for course in courses:
         small_course=(course.id,course.course_name)
         course_list.append(small_course)
+    #course_list=[]
+
+    session_list = []
+    sessions = SessionYearModel.object.all()
+
+    for ses in sessions:
+        small_ses = (ses.id, str(ses.session_start_year)+"   TO  "+str(ses.session_end_year))
+        session_list.append(small_ses)
 
     gender_choice=(
         ("Male","Male"),
@@ -26,8 +35,7 @@ class AddStudentForm(forms.Form):
 
     course=forms.ChoiceField(label="Course",choices=course_list,widget=forms.Select(attrs={"class":"form-control"}))
     sex=forms.ChoiceField(label="Sex",choices=gender_choice,widget=forms.Select(attrs={"class":"form-control"}))
-    session_start=forms.DateField(label="Session Start",widget=DateInput(attrs={"class":"form-control"}))
-    session_end=forms.DateField(label="Session End",widget=DateInput(attrs={"class":"form-control"}))
+    session_year_id=forms.ChoiceField(label="Session Year",choices=session_list,widget=forms.Select(attrs={"class":"form-control"}))
     profile_pic=forms.FileField(label="Profile Pic",max_length=50,widget=forms.FileInput(attrs={"class":"form-control"}))
 
 class EditStudentForm(forms.Form):
@@ -37,11 +45,26 @@ class EditStudentForm(forms.Form):
     username=forms.CharField(label="Username",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
     address=forms.CharField(label="Address",max_length=50,widget=forms.TextInput(attrs={"class":"form-control"}))
 
-    courses=Courses.objects.all()
+
     course_list=[]
-    for course in courses:
-        small_course=(course.id,course.course_name)
-        course_list.append(small_course)
+    try:
+        courses = Courses.objects.all()
+        for course in courses:
+            small_course=(course.id,course.course_name)
+            course_list.append(small_course)
+    except:
+        course_list=[]
+
+    session_list = []
+    try:
+        sessions = SessionYearModel.object.all()
+
+        for ses in sessions:
+            small_ses = (ses.id, str(ses.session_start_year)+"   TO  "+str(ses.session_end_year))
+            session_list.append(small_ses)
+    except:
+        pass
+        #session_list = []
 
     gender_choice=(
         ("Male","Male"),
@@ -50,6 +73,5 @@ class EditStudentForm(forms.Form):
 
     course=forms.ChoiceField(label="Course",choices=course_list,widget=forms.Select(attrs={"class":"form-control"}))
     sex=forms.ChoiceField(label="Sex",choices=gender_choice,widget=forms.Select(attrs={"class":"form-control"}))
-    session_start=forms.DateField(label="Session Start",widget=DateInput(attrs={"class":"form-control"}))
-    session_end=forms.DateField(label="Session End",widget=DateInput(attrs={"class":"form-control"}))
+    session_year_id=forms.ChoiceField(label="Session Year",choices=session_list,widget=forms.Select(attrs={"class":"form-control"}))
     profile_pic=forms.FileField(label="Profile Pic",max_length=50,widget=forms.FileInput(attrs={"class":"form-control"}),required=False)
--- student_management_app/models.py
+++ student_management_app/models.py
@@ -4,6 +4,12 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 # Create your models here.
+class SessionYearModel(models.Model):
+    id=models.AutoField(primary_key=True)
+    session_start_year=models.DateField()
+    session_end_year=models.DateField()
+    object=models.Manager()
+
 class CustomUser(AbstractUser):
     user_type_data=((1,"HOD"),(2,"Staff"),(3,"Student"))
     user_type=models.CharField(default=1,choices=user_type_data,max_length=10)
@@ -46,8 +52,7 @@ class Students(models.Model):
     profile_pic=models.FileField()
     address=models.TextField()
     course_id=models.ForeignKey(Courses,on_delete=models.DO_NOTHING)
-    session_start_year=models.DateField()
-    session_end_year=models.DateField()
+    session_year_id=models.ForeignKey(SessionYearModel,on_delete=models.CASCADE)
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
@@ -57,6 +62,7 @@ class Attendance(models.Model):
     subject_id=models.ForeignKey(Subjects,on_delete=models.DO_NOTHING)
     attendance_date=models.DateTimeField(auto_now_add=True)
     created_at=models.DateTimeField(auto_now_add=True)
+    session_year_id=models.ForeignKey(SessionYearModel,on_delete=models.CASCADE)
     updated_at=models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
 
@@ -136,7 +142,7 @@ def create_user_profile(sender,instance,created,**kwargs):
         if instance.user_type==2:
             Staffs.objects.create(admin=instance,address="")
         if instance.user_type==3:
-            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_start_year="2020-01-01",session_end_year="2021-01-01",address="",profile_pic="",gender="")
+            Students.objects.create(admin=instance,course_id=Courses.objects.get(id=1),session_year_id=SessionYearModel.object.get(id=1),address="",profile_pic="",gender="")
 
 @receiver(post_save,sender=CustomUser)
 def save_user_profile(sender,instance,**kwargs):
